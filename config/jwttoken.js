bc764b7916cc824a96fb4d448d9a0ff476571bbe
lepakshiadni <144108879+lepakshiadni@users.noreply.github.com>
2024-05-31 11:48:31 +0530

    Delete config directory

--- config/jwttoken.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (_id) => {
-  return jwt.sign({ _id }, process.env.JWT_TOKEN, { expiresIn: "1d" });
-};
-
-module.exports = { generateToken };
